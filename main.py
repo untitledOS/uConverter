a459ec4404ace4cd91167d312387dddb0d59c3ba
yuck <60288171+yuckdevchan@users.noreply.github.com>
2023-03-20 11:26:43 +0000

    Add textbox

--- main.py
+++ main.py
@@ -10,17 +10,23 @@ class YTDDMainWindow(QtWidgets.QWidget):
         super().__init__()
 
         self.button = QtWidgets.QPushButton("Download")
-        self.text = QtWidgets.QLabel("Choose a URL:", alignment=QtCore.Qt.AlignTop)
+        self.text = QtWidgets.QLabel("Trapeze YouTube Downloader", alignment=QtCore.Qt.AlignTop)
+        
+        # User Input
+        self.url_input_box = QtWidgets.QLineEdit(self)
+        self.url_input_box.setPlaceholderText("Enter a URL...")
+        self.open_button = QtWidgets.QPushButton("Open")
+        self.quit_button = QtWidgets.QPushButton("Quit")
 
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.text)
         self.layout.addWidget(self.button)
-        self.setWindowTitle("YouTube Downloader Deluxe")
+        self.setWindowTitle("Trapeze YouTube Downloader")
 
-        self.button.clicked.connect(self.download)
+        self.button.clicked.connect(self.yt_download)
 
     @QtCore.Slot()
-    def download(self):
+    def yt_download(self):
         pass
 
 if __name__ == "__main__":
